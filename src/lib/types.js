1b782350030ef5aec38aaa0ca7506ee41aa8cedc
Ilya <iosdsbb@gmail.com>
2025-09-16 09:09:59 +0300

    types(kv-storage): TodoTask impl SerializableObject, move TodoOptions

--- src/lib/types.js
+++ src/lib/types.js
@@ -4,7 +4,16 @@
  */
 
 /**
+ * @typedef {Object} TodoOptions
+ * @property {KVStorageEngine} storage - Движок хранилища для сохранения задач
+ * @property {HTMLElement} [root] - DOM элемент-контейнер для рендера todo-списка
+ * @property {string} [storageKey='todos'] - Ключ для сохранения данных в хранилище
+ * @property {boolean} [autoSave=true] - Автоматически сохранять изменения
+ */
+
+/**
  * @typedef {Object} TodoTask
+ * @implements {SerializableObject}
  * @property {string} id - Уникальный идентификатор
  * @property {string} title - Заголовок задачи
  * @property {string} [description] - Описание задачи
@@ -14,4 +23,5 @@
  * @property {string[]} [tags] - Теги для категоризации
  * @property {string} [category] - Категория задачи
  * @property {Object} [metadata] - Дополнительные метаданные
+ * @property {function(): SerializableVal} toJSON - Метод для сериализации задачи в JSON
  */
