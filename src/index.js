271209d50bd5f85e3973fbd8482629528bd01414
Ilya <iosdsbb@gmail.com>
2025-09-16 19:57:13 +0300

    chore(ee): testing

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 import {LocalStorageEngine, Todo} from "~lib/index.js";
 import {Form} from "~lib/widgets/form/index.js";
+import EventEmitter from "~core/event-emitter/index.js";
 
-const $root = document.getElementById("root");
-const $container = document.getElementsByClassName("container")[0];
-console.log($container);
+// const $root = document.getElementById("root");
+// const $container = document.getElementsByClassName("container")[0];
+// console.log($container);
+//
+// const todo = new Todo({
+//   storage: new LocalStorageEngine(),
+//   storageKey: "todo",
+//   root: $container,
+//   autoSave: true,
+// });
+//
+// const form = new Form({
+//   formClass: "form",
+//   todo,
+// });
 
-const todo = new Todo({
-  storage: new LocalStorageEngine(),
-  storageKey: "todo",
-  root: $container,
-  autoSave: true,
-});
+const ee = new EventEmitter();
 
-const form = new Form({
-  formClass: "form",
-  todo,
-});
+function log1() {
+  console.log(1);
+}
+
+function log2() {
+  console.log(2);
+}
+
+function log3() {
+  console.log(3);
+}
+
+ee.on('log', log1);
+ee.on('log', log2);
+ee.on('log', log3);
+
+ee.off('log', log3);
+ee.emit('log');
