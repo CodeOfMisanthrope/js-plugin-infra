8b3567250447f9cce637330553f456517794c7c1
Ilya <iosdsbb@gmail.com>
2025-09-16 21:24:47 +0300

    types(ee): updated FormOptions

--- src/lib/widgets/form/types.js
+++ src/lib/widgets/form/types.js
@@ -4,4 +4,5 @@
  * @property {Todo} todo - Экземпляр класса Todo для управления задачами
  * @property {string} [formClass='todo-form'] - CSS класс для формы
  * @property {boolean} [clearOnSubmit=true] - Очищать поле после отправки
+ * @property {string} ee - EventEmitter
  */
