5b1f1e5d1bf2e394d89df680c7fda8ed802ff2a5
Ilya <iosdsbb@gmail.com>
2025-09-16 18:30:03 +0300

    feat(todo): added Form

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import {LocalStorageEngine, Todo} from "~lib/index.js";
+import {Form} from "~lib/widgets/form/index.js";
 
 const $root = document.getElementById("root");
-console.log(22)
+const $container = document.getElementsByClassName("container")[0];
+console.log($container);
+
 const todo = new Todo({
   storage: new LocalStorageEngine(),
   storageKey: "todo",
-  root: $root,
+  root: $container,
   autoSave: true,
+});
+
+const form = new Form({
+  formClass: "form",
+  todo,
 });
---
+++ src/lib/widgets/form/index.js
@@ -0,0 +1,67 @@
+import TodoTask from "~lib/task.js";
+
+/**
+ * Класс для управления формами
+ * @class Form
+ */
+export class Form {
+  /** @type {HTMLElement} */
+  #root;
+  /** @type {string} */
+  #formClass;
+  /** @type {HTMLInputElement[]} */
+  #inputs;
+  /** @type {HTMLFormElement} */
+  #form;
+  /** @type {Todo} */
+  #todo;
+
+  /**
+   * Создает экземпляр Form
+   * @param {FormOptions} opts - Объект опций
+   */
+  constructor(opts) {
+    this.#root = opts.root;
+    this.#formClass = opts.formClass;
+    this.#inputs = [...document.getElementsByClassName('form__input')];
+    // console.log(this.#inputs);
+    this.#form = document.getElementsByClassName('form')[0];
+    this.#todo = opts.todo;
+    // console.log(this.#form);
+    this.#initSubmit();
+  }
+
+  #initSubmit() {
+    this.#form.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      if (this.validate()) {
+        this.submit();
+      }
+    });
+  }
+
+  validate() {
+    // todo Сделать только для name
+    for (const input of this.#inputs) {
+      const {value} = input;
+      if (value === "") {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  submit() {
+    console.log('submit');
+    const taskData = {};
+    for (const input of this.#inputs) {
+      console.log(input.name);
+      const {name,value} = input;
+      taskData[name] = value;
+    }
+    console.log(taskData)
+    const newTask = new TodoTask(taskData);
+    this.#todo.addTask(newTask)
+  }
+}
