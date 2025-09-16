b70bc0e0548575fd2ed9e89324be6943eb9ed3a1
Ilya <iosdsbb@gmail.com>
2025-09-16 21:41:14 +0300

    feat(ee): updated TodoList

--- src/lib/widgets/list/index.js
+++ src/lib/widgets/list/index.js
@@ -1,5 +1,23 @@
-class List {
+class TodoList {
+  /** @type {HTMLElement} */
+  #root;
+  /** @type {EventEmitter} */
+  #ee;
+
+  /**
+   * Создает экземпляр TodoList
+   * @param {TodoListOptions} opts - Объект опций
+   */
   constructor(opts) {
-    
+    this.#root = opts.root;
+    this.#ee = opts.ee;
+  }
+
+  addTask(num) {
+
+  }
+
+  removeTask(num) {
+
   }
 }
