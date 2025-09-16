4c07531947df894b4b7140ad385fa808b3e08ecb
Ilya <iosdsbb@gmail.com>
2025-09-16 17:58:09 +0300

    types(form): added FormOptions

---
+++ src/lib/widgets/form/types.js
@@ -0,0 +1,7 @@
+/**
+ * @typedef {Object} FormOptions
+ * @property {HTMLElement} root - DOM элемент-контейнер для рендера формы
+ * @property {Todo} todo - Экземпляр класса Todo для управления задачами
+ * @property {string} [formClass='todo-form'] - CSS класс для формы
+ * @property {boolean} [clearOnSubmit=true] - Очищать поле после отправки
+ */
