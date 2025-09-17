c4e28fbe893b7d80571c55b6cb53f038fe852a12
Ilya <iosdsbb@gmail.com>
2025-09-17 08:22:46 +0300

    types(list): updated TodoListOptions

--- src/lib/widgets/list/types.js
+++ src/lib/widgets/list/types.js
@@ -1,5 +1,7 @@
 /**
  * @typedef {Object} TodoListOptions
  * @property {HTMLElement} [root] - DOM элемент-контейнер для рендера todo-списка
+ * @property {HTMLElement} listClass - DOM элемент-контейнер для рендера todo-списка
+ * @property {Todo} todo - Todo
  * @property {EventEmitter} ee - EventEmitter
  */
