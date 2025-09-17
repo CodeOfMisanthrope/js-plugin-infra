43f5c47171460124031b5b7d26dfd1a5bd80e65a
Ilya <iosdsbb@gmail.com>
2025-09-17 09:17:11 +0300

    chore(task): testing list

--- src/index.js
+++ src/index.js
@@ -1,40 +1,51 @@
 import {LocalStorageEngine, Todo} from "~lib/index.js";
 import {Form} from "~lib/widgets/form/index.js";
 import EventEmitter from "~core/event-emitter/index.js";
+import TodoList from "~lib/widgets/list/index.js";
 
-// const $root = document.getElementById("root");
-// const $container = document.getElementsByClassName("container")[0];
-// console.log($container);
-//
-// const todo = new Todo({
-//   storage: new LocalStorageEngine(),
-//   storageKey: "todo",
-//   root: $container,
-//   autoSave: true,
-// });
-//
-// const form = new Form({
-//   formClass: "form",
-//   todo,
-// });
+const $root = document.getElementById("root");
+const $container = document.getElementsByClassName("container")[0];
+console.log($container);
 
 const ee = new EventEmitter();
 
-function log1() {
-  console.log(1);
-}
+const todo = new Todo({
+  storage: new LocalStorageEngine(),
+  storageKey: "todo",
+  root: $container,
+  autoSave: true,
+  ee,
+});
 
-function log2() {
-  console.log(2);
-}
+const form = new Form({
+  formClass: "form",
+  todo,
+  ee
+});
 
-function log3() {
-  console.log(3);
-}
+const list = new TodoList({
+  listClass: document.getElementById("task__list"),
+  todo,
+  ee,
+});
 
-ee.on('log', log1);
-ee.on('log', log2);
-ee.on('log', log3);
-
-ee.off('log', log3);
-ee.emit('log');
+// const ee = new EventEmitter();
+//
+// function log1() {
+//   console.log(1);
+// }
+//
+// function log2() {
+//   console.log(2);
+// }
+//
+// function log3() {
+//   console.log(3);
+// }
+//
+// ee.on('log', log1);
+// ee.on('log', log2);
+// ee.on('log', log3);
+//
+// ee.off('log', log3);
+// ee.emit('log');
